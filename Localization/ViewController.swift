c2de74ebeca215a63b8eb5198f6b43ffb8433a45
RodrigoOkido <rodrigookido@gmail.com>
2021-09-18 23:22:37 -0300

    Simple localization demo

--- Localization/ViewController.swift
+++ Localization/ViewController.swift
@@ -9,9 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var navigationTitle: UINavigationItem!
+    
+    @IBOutlet weak var flagImage: UIImageView!
+    @IBOutlet weak var countryName: UILabel!
+    @IBOutlet weak var language: UILabel!
+    
+    @IBOutlet weak var localizationDescription: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+
+        navigationTitle.title = NSLocalizedString("navTitle", comment: "")
+
+        flagImage.image = UIImage(named: NSLocalizedString("flag", comment: ""))
+        countryName.text = NSLocalizedString("countryName", comment: "")
+        language.text = NSLocalizedString("language", comment: "")
+        
+        localizationDescription.text = NSLocalizedString("aboutLocalization", comment: "")
     }
 
 
